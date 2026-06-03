02a753aefe21ab857b629ac7e5c5477ad0b3cd0a
FhussainCodes <farrukhhussainofficial13@gmail.com>
2026-06-03 11:12:01 +0500

    initialize empty controller, routes, and blade views

--- app/Models/ChangeRequest.php
+++ app/Models/ChangeRequest.php
@@ -7,6 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class ChangeRequest extends Model
 {
     protected $fillable = [
-
+        "request_no",
+        "project_module",
+        "department",
+        "requested_by",
+        "priority",
+        "problem_statement",
+        "decision",
+        "comments",
+        "request_type",
+        "change_type",
+        "assigned_to",
+        "assigned_date",
+        "assigned_by",
+        "uat_by",
+        "deployed_by",
+        "version"
     ];
 }
