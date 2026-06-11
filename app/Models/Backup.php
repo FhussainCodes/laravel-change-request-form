1cdd6d0a4b4e7cc18ab547af9f9053c26f874d8b
FhussainCodes <betterpart2300@gmail.com>
2026-06-11 16:23:32 +0500

    define mass-assignable fillable attributes in backup model and link storage

--- app/Models/Backup.php
+++ app/Models/Backup.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Backup extends Model
 {
-    //
+        use HasFactory;
+
+    // Defines the database columns that can be filled by our form
+    protected $fillable = [
+        'backup_type',
+        'backup_datetime',
+        'image', 
+        'created_by',
+    ];
 }
